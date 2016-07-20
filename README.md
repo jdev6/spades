ecbcb19220a1af612dd3dbce8972f1293062747e
jdev6 <jdev@gmail.com>
2016-07-20 17:31:17 +0200

    Configuration files, updated documentation, prompt

--- README.md
+++ README.md
@@ -2,6 +2,91 @@
 
 Text editor built with [electron](http://electron.atom.io/) and [ace](https://ace.c9.io)
 
-Currently you can only edit files, but you can't change font, theme, etc.
+#Configuration
 
-This is a wip.
+The path in which configuration files will be stored is:
+            Windows: %appdata%/spades
+            Everyting else: $HOME/.spades
+
+Configuration files use the json format:
+
+    style.json: {
+        "theme": "an ace theme",
+        "css": {
+            //custom css here
+            //for example:
+            fontSize: "14px"
+        }
+    }
+
+    keymaps.json: {
+        //$C is used for Command in Mac and Ctrl in any other os
+        "file-open": "$C-O",
+        "file-save": "$C-S",
+        "prompt-open": "$C-Space", //See #Prompt below
+        "prompt-exec": "Enter",
+        "build": "$C-B",
+        "reload": "$C-R"
+    }
+
+    builds.json: {
+        //Build systems
+        //Example:
+        "npm": "npm start run"    
+    }
+
+#Prompt
+
+Spades features a prompt/infobar to issue commands.
+
+You can access the prompt by pressing Ctrl/Command - Space (default keybinding)
+
+The commands are:
+
+    help:              display this message
+    set [var] [value]: set the value of var
+    get [var]:         get the value of var
+    list [var]:        list all possible options for var
+    build              builds the project using the current build system
+
+Example:
+
+    list theme
+    [LIST] Options:
+    ambiance
+    chaos
+    chrome
+    clouds
+    clouds_midnight
+    cobalt
+    crimson_editor
+    dawn
+    dreamweaver
+    eclipse
+    github
+    idle_fingers
+    iplastic
+    katzenmilch
+    kr_theme
+    kuroir
+    merbivore
+    merbivore_soft
+    mono_industrial
+    monokai
+    pastel_on_dark
+    solarized_dark
+    solarized_light
+    sqlserver
+    terminal
+    textmate
+    tomorrow
+    tomorrow_night_blue
+    tomorrow_night_bright
+    tomorrow_night_eighties
+    tomorrow_night
+    twilight
+    vibrant_ink
+    xcode
+
+    set mode javascript
+    //Javascript syntax highlighting
--- spades.js
+++ spades.js
@@ -1,18 +1,14 @@
 /* REQUIRES */
 let fs = require('fs'); //File system module
 let path = require('path'); //Path module
+let spawn = require('child_process').spawn; //For builds
 
-let {ipcRenderer} = require('electron'); //IpcRenderer (to communicate with the main process)
+let ipcRenderer = require('electron').ipcRenderer; //IpcRenderer (to communicate with the main process)
 
 let fileExtensions = require('./file-extensions.json'); //File extensions associated with syntax modes
+let execPromptCmd = require('./prompt-commands.js');
 
 /* HELPER FUNCTIONS */
-let logstdout = (what) => {
-    //prints to electron console and stdout (chromium console and terminal)
-    process.stdout.write(what + '\n');
-    console.log(what);
-};
-
 let $id = (id) => document.getElementById(id);
 
 fs.existsSync = (path) => {
@@ -28,7 +24,7 @@ fs.existsSync = (path) => {
 /*COMMANDS*/
 let commands = {
     'file-open': {
-        exec: (editor) => {
+        exec: () => {
             //Send file open signal to main process
             ipcRenderer.send('file-open');
         },
@@ -36,42 +32,69 @@ let commands = {
     },
     
     'file-save': {
-        exec: (editor) => {
+        exec: () => {
             //Save file
             if (spades.hasFile) {
                 let err = spades.file.save(spades.editor.getValue());
                 if (err)
-                    logstdout("Couldn't save file: " + err);
+                    spades.log("Couldn't save file: " + err, "ERROR");
             }
         },
         readOnly: false
     },
     
-    'reload': {
-        exec: (editor) => {
-            //Reload config
-            spades.loadConfig('*');
-        },
-        readOnly: true
-    }/*,
+    'prompt-open': {
+        exec: () => {
+            //Open prompt
+            spades.inPrompt = true;
+            spades.infoBar.setReadOnly(false);
+            spades.infoBar.focus();
+            spades.infoBar.navigateFileEnd();
+            spades.infoBar.scrollPageDown();
 
-    'file-next': {
-        exec: (editor) => {
-            //Next file
-            logstdout("Next "+ spades.fileNum+1);
-            spades.fileNum++;
-            spades.editor.setValue(spades.currentFile.read());
         },
         readOnly: true
     },
 
-    'file-prev': {
-        exec: (editor) => {
-            //Previous file
-            spades.fileNum--;
+    'prompt-exec': {
+        exec: () => {
+            //Execute command in prompt
+            if (spades.inPrompt) {
+                let cmd = spades.infoBar.getSession() //Get contents of last line
+                    .getLine(spades.infoBar.getSession().getLength()-1);
+
+                spades.infoBar.setValue(spades.infoBar.getValue() + '\n');
+
+                //Exit prompt
+                spades.inPrompt = false;
+                spades.infoBar.setReadOnly(true);
+
+                //Execute prompt command
+                let err = execPromptCmd(cmd, spades);
+                if (err)
+                    spades.log(err, "ERROR");
+            }
+
+            else if (spades.editor.isFocused())
+                spades.editor.insert('\n');
         },
         readOnly: true
-    }*/
+    },
+
+    'build': {
+        exec: () => {
+            spades.doBuild();
+        },
+        readOnly: true
+    },
+
+    'reload': {
+        exec: () => {
+            //Reload config
+            spades.loadConfig('*');
+        },
+        readOnly: true
+    }
 }
 
 /*CLASS DECLARATIONS*/
@@ -91,13 +114,13 @@ class File {
     read() {
         /*
         let data = this.readStream.read(this.size);
-        logstdout(data);
+        spades.log(data);
         */
         let data;
         try {
             data = fs.readFileSync(this.path, {encoding: 'utf8'});
         } catch(err) {
-            logstdout("Error reading file: " + err);
+            spades.log("Error reading file: " + err, "ERROR");
             throw err;
         }
         return data;
@@ -126,28 +149,47 @@ let spades; //spades global instance
 class Spades {
     //Main spades editor class
     constructor() {
-        logstdout("Config file path is " + this.configPath);
+        this.log("Config file path is " + this.configPath);
 
         this.hasFile = false;
+        this.inPrompt = false;
 
         this.editor = ace.edit("editor");
         this.editor.setReadOnly(true);
         this.editor.$blockScrolling = Infinity;
+        this.editor.getSession().setUseWrapMode(true);
 
-        /*this.infoBar = ace.edit("infobar");
+        this.infoBar = ace.edit("infobar");
         this.infoBar.setReadOnly(true);
-        $id("infobar").style.position = "absolute";
-        $id("infobar").style.top = 60
-        $id("infobar").style.height = "20px";
-        $id("infobar").style.width = this.editor.getSession().getScreenWidth();*/
+        this.infoBar.getSession().setUseWrapMode(true);
+        this.infoBar.$blockScrolling = Infinity;
+        this.infoBar.setScrollSpeed(0.5);
+
+        this.modeList = [];
+        this.themeList = [];
+
+        fs.readdir("ace-min-noconflict", (err, files) => {
+            if (err)
+                return spades.log("Cannot read ace directory", "ERROR");
+
+            files.forEach((f) => {
+                if (/mode-/.test(f))
+                    this.modeList.push(f.replace(/mode-(.+)\.js/, "$1"));
+
+                else if (/theme-/.test(f))
+                    this.themeList.push(f.replace(/theme-(.*)\.js/, "$1"));
+
+            });
+        });
 
         this.configDefaults = {
             keymaps: {
-                'file-open': '$C-O',
-                'file-save': '$C-S',
-                'reload': '$C-R'/*,
-                'file-next': '$C-Tab',
-                'file-prev': '$C-Shift-Tab',*/
+                "file-open": "$C-O",
+                "file-save": "$C-S",
+                "prompt-open": "$C-Space",
+                "prompt-exec": "Enter",
+                "build": "$C-B",
+                "reload": "$C-R"
             },
             style: {},
             builds: {}
@@ -155,23 +197,28 @@ class Spades {
 
         this.loadConfig('*'); //Load all configuration
 
-        for (let cmd in commands) {
-            this.editor.commands.addCommand({
-                name: cmd,
+        for (let name in commands) {
+            //Add commands
+            let cmd = {
+                name: name,
                 bindKey: {
-                    win: this.keymaps[cmd].replace('$C', 'Ctrl'),
-                    mac: this.keymaps[cmd].replace('$C', 'Command')
+                    win: this.keymaps[name].replace('$C', 'Ctrl'),
+                    mac: this.keymaps[name].replace('$C', 'Command')
                 },
-                exec: commands[cmd].exec,
-                readOnly: commands[cmd].readOnly
-            });
+                exec: commands[name].exec,
+                readOnly: commands[name].readOnly
+            }
+
+            this.editor.commands.addCommand(cmd);
+            this.infoBar.commands.addCommand(cmd);
         }
 
-        setInterval((self) => {
-            if (self.hasFile && !spades.fileSaved)
-                document.title = `${self.filePath} * - spades`;
+        setInterval((_this) => {
+            //Checks if file is saved
+            if (_this.hasFile && !_this.fileSaved)
+                document.title = `${_this.filePath} * - spades`;
             else
-                document.title = `${self.filePath} - spades`;
+                document.title = `${_this.filePath} - spades`;
         }, 1000, this);
     }
 
@@ -190,27 +237,33 @@ class Spades {
 
         let configFile = path.join(this.configPath, cfgName + '.json');
 
-        let cfg;
+        let cfg = this.configDefaults[cfgName]; //Load default configuration
 
-        if (!fs.existsSync(configFile)) {
+        if (!fs.existsSync(configFile))
             //File doesn't exist
-            cfg = this.configDefaults[cfgName];
             fs.writeFileSync(configFile, JSON.stringify(cfg));
 
-        } else {
+        else {
             //File exists
             let data = fs.readFileSync(configFile, {encoding: 'utf8'});
             try {
-                cfg = JSON.parse(data);
+                //Overwrite default values
+                let json = JSON.parse(data);
+                let keys = Object.keys(json);
+                keys.forEach((key) => {
+                    cfg[key] = json[key];
+                });
+
             } catch(err) {
-                throw `${err} (in ${cfgName})`;
+                //Error while parsing Jason
+                this.log(`${err} (in ${cfgName}.json)`, "ERROR");
             }
         }
 
         switch (cfgName) {
             case "style":
                 let theme = `ace/theme/${cfg.theme}`;
-                logstdout("Set theme " + theme);
+                this.log("Set theme " + theme);
                 this.setTheme(theme);
 
                 //Set css
@@ -220,18 +273,65 @@ class Spades {
                     for(let s in cfg.css){
                         style[s] = cfg.css[s];
                     }
+
+                    $id("infobar").style.fontSize = style.fontSize;
                 }
                 break;
 
             case "keymaps":
                 this.keymaps = cfg;
                 break;
+
+            case "builds":
+                this.builds = cfg;
+                this.build = Object.keys(this.builds)[0];
+                break;
         }
+    }
+
+    doBuild() {
+        let cmd = this.buildCmd.replace("$f", this.filePath).split(" ");
+        let arg = cmd.splice(1);
+
+        let child = spawn(cmd[0], arg);
+
+        child.on('error', (err) => {
+            this.log(err, "BUILD");
+        });
+
+        child.stdout.on('data', (data) => {
+            this.log(data, "BUILD");
+        });
+
+        child.stderr.on('data', (data) => {
+            this.log(data, "BUILD");
+        });
+
+        child.on('close', (code) => {
+            this.log(`${cmd} finished with exit code ${code}`, "BUILD");
+        })
     }
 
     setTheme(path) {
         this.editor.setTheme(path);
-        //this.infoBar.setTheme(path);
+        this.infoBar.setTheme(path);
+    }
+
+    log(what, type) {
+        //prints to electron console, stdout and infobar
+        let msg = `[${type || "INFO"}] ${what}`
+        process.stdout.write(msg + '\n');
+        console.log(msg);
+        if (this.infoBar) {
+            this.infoBar.setValue(this.infoBar.getValue() + msg + '\n');
+            //Scroll to bottom
+            this.infoBar.navigateFileEnd();
+            this.infoBar.scrollToRow(this.infoBar.getLastVisibleRow());
+        }
+    }
+
+    get buildCmd() {
+        return this.builds[this.build];
     }
 
     get filePath() {
@@ -256,7 +356,7 @@ class Spades {
 
 ipcRenderer.on('file-opened', (event, filename) => {
     //When the main process has opened a file
-    logstdout("Open file: " + filename);
+    spades.log("Open file: " + filename);
 
     spades.file = new File(filename);
     spades.hasFile = true;
@@ -273,10 +373,10 @@ ipcRenderer.on('file-opened', (event, filename) => {
         ? matches[0].substring(1) //Remove the dot
         : "txt";
 
-    let mode = "ace/mode/" + (fileExtensions[fileExtension] || "plain_text");
+    let mode = fileExtensions[fileExtension] || "plain_text";
 
-    logstdout("Set mode " + mode);
-    spades.editor.getSession().setMode(mode);
+    spades.log("Set mode " + mode);
+    spades.editor.getSession().setMode("ace/mode/" + mode);
 });
 
 (() => {
